c58d643e893305cdd1a27df0b1aeff81b415faac
kostyakostya61 <kvoronin.exceedteam@gmail.com>
2020-12-03 16:58:35 +0300

    first step

--- projects/gatsby-app/account-backend-system/utils/jwtGenerator.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-const config = require('config');
-require('dotenv').config();
-
-function jwtGenerator(obj) {
-  const object = { obj };
-  return jwt.sign(object, config.get('jwtSecret'), { expiresIn: '1h' });
-}
-module.exports = jwtGenerator;
